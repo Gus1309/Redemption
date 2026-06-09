950a326ca4919c3538f0a6bcfdabc0fb5af8bd4a
AgusMig <agusmig05@gmail.com>
2026-06-09 20:32:37 -0300

    Jerarquía de uduarios

---
+++ src/main/java/com/barrios/modelo/Propietario.java
@@ -0,0 +1,21 @@
+package com.barrios.modelo;
+
+public class Propietario extends Usuario {
+    private String telefono;
+
+    public Propietario() {
+    }
+
+    public Propietario(Long id, String nombre, String email, String telefono) {
+        super(id, nombre, email);
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ src/main/java/com/barrios/modelo/Tecnico.java
@@ -0,0 +1,21 @@
+package com.barrios.modelo;
+
+public class Tecnico extends Usuario {
+    private String especialidad;
+
+    public Tecnico() {
+    }
+
+    public Tecnico(Long id, String nombre, String email, String especialidad) {
+        super(id, nombre, email);
+        this.especialidad = especialidad;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+}
