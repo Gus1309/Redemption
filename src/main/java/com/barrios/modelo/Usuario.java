29b00e95712a7707f37ff6c8d643335ac39951d4
AgusMig <agusmig05@gmail.com>
2026-06-16 19:26:13 -0300

    mejora visual front

--- src/main/java/com/barrios/modelo/Usuario.java
+++ src/main/java/com/barrios/modelo/Usuario.java
@@ -3,7 +3,10 @@ package com.barrios.modelo;
 public abstract class Usuario {
     private Long id;
     private String nombre;
+    private String apellido;
     private String email;
+    private String password;
+    private String rolTexto;
 
     public Usuario() {
     }
@@ -12,6 +15,15 @@ public abstract class Usuario {
         this.id = id;
         this.nombre = nombre;
         this.email = email;
+    }
+
+    public Usuario(Long id, String nombre, String apellido, String email, String password, String rolTexto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.rolTexto = rolTexto;
     }
 
     public Long getId() {
@@ -30,12 +42,36 @@ public abstract class Usuario {
         this.nombre = nombre;
     }
 
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRolTexto() {
+        return rolTexto;
+    }
+
+    public void setRolTexto(String rolTexto) {
+        this.rolTexto = rolTexto;
     }
 
     public abstract Rol getRol();
